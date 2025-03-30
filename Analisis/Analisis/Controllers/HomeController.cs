7c762e7746cf659d6f7914d4783f6b0fead14a78
HeymmyLJuarez <hlopez68900@ufide.ac.cr>
2025-03-30 12:24:54 -0600

    Configuracion de pantallas
    Se genera el Layout, se agregan pantallas de módulos de recibos, ventas y usuarios

---
+++ Analisis/Analisis/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Analisis.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Acción para la vista de Crear Usuario
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult ConfirmEdit()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmDelete()
+        {
+            return View();
+        }
+        public ActionResult Users()
+        {
+            return View();
+        }
+        public ActionResult Sales()
+        {
+            return View();
+        }
+        public ActionResult ErrorSale()
+        {
+            return View();
+        }
+        public ActionResult ConfirmSale()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterSale()
+        {
+            return View();
+        }
+        public ActionResult ListSales()
+        {
+            return View();
+        }
+        public ActionResult ErrorPermission()
+        {
+            return View();
+        }
+        public ActionResult Receipts()
+        {
+            return View();
+        }
+        public ActionResult ConfirmReceiptUpdate()
+        {
+            return View();
+        }
+        public ActionResult EditReceipt()
+        {
+            return View();
+        }
+        public ActionResult ListReceipts()
+        {
+            return View();
+        }
+        public ActionResult CancelReceipt()
+        {
+            return View();
+        }
+        public ActionResult ConfirmCancelReceipt()
+        {
+            return View();
+        }
+        public ActionResult ListAnular()
+        {
+            return View();
+        }
+    }
+
+}
