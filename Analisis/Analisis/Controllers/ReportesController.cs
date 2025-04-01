4b1d295e20c67ed03bd3e7591f0689b51d108fbf
jafet0312 <114125171+jafet0312@users.noreply.github.com>
2025-04-01 11:56:34 -0600

    Vistas inventario y reportes

---
+++ Analisis/Analisis/Controllers/ReportesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+
+namespace Analisis.Controllers
+{
+    public class ReportesController : Controller
+    {
+        // Página principal de reportes
+        public ActionResult Index()
+        {
+            return View("~/Views/Reportes/Reportes.cshtml");
+        }
+
+        // Acción para generar reportes de ventas
+        public ActionResult ReporteVentas()
+        {
+            return View();
+        }
+
+        // Acción para ver productos más vendidos
+        public ActionResult ProductosMasVendidos()
+        {
+            return View();
+        }
+
+        // Acción para ver el estado del stock
+        public ActionResult EstadoStock()
+        {
+            return View();
+        }
+
+        // Acción para ver historial de compras por cliente
+        public ActionResult HistorialComprasCliente()
+        {
+            return View();
+        }
+
+        // Acción para ver historial de servicios por profesional
+        public ActionResult HistorialServiciosProfesional()
+        {
+            return View();
+        }
+    }
+}
