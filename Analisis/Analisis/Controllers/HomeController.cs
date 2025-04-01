1cfa3e3df4ed40a08cf04093dde2ee6048efe6fb
jafet0312 <114125171+jafet0312@users.noreply.github.com>
2025-04-01 11:38:29 -0600

    Vista inventario y reportes

--- Analisis/Analisis/Controllers/HomeController.cs
+++ Analisis/Analisis/Controllers/HomeController.cs
@@ -98,6 +98,8 @@ namespace Analisis.Controllers
         {
             return View();
         }
+
     }
+
 
 }
---
+++ Analisis/Analisis/Controllers/InventarioController.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Mvc;
+
+namespace Analisis.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // Página principal del inventario
+        public ActionResult Index()
+        {
+            return View("~/Views/Home/Inventario.cshtml");
+        }
+
+        // Acción para registrar un nuevo producto
+        public ActionResult RegistrarProducto()
+        {
+            return View();
+        }
+
+        // Acción para editar un producto existente
+        public ActionResult EditarProducto(int id)
+        {
+            ViewBag.ProductoId = id;
+            return View();
+        }
+
+        // Acción para registrar ingreso de stock
+        public ActionResult RegistrarIngreso()
+        {
+            return View();
+        }
+
+        // Acción para registrar salida de stock
+        public ActionResult RegistrarSalida()
+        {
+            return View();
+        }
+
+        // Acción para ajustar stock manualmente
+        public ActionResult AjustarStock()
+        {
+            return View();
+        }
+
+        // Acción para ver el historial de movimientos
+        public ActionResult HistorialMovimientos()
+        {
+            return View();
+        }
+
+        // Acción para mostrar alertas de stock mínimo
+        public ActionResult AlertasStock()
+        {
+            return View();
+        }
+    }
+}
