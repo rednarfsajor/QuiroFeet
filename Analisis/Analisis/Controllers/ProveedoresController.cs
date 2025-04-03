4607c5323010f041a9ee52d542e451d7d4ce06ed
jseara118 <jchavarria670331@ufide.ac.cr>
2025-04-02 19:39:09 -0600

    Vistas Modulos Proveedores Y Profesionales
    Vistas creadas y funcionales con mismo formato.

---
+++ Analisis/Analisis/Controllers/ProveedoresController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace Analisis.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // Página principal de empleados
+        public ActionResult Index()
+        {
+            return View("~/Views/Proveedores/Proveedores.cshtml");
+        }
+
+        // Acción registrar empleado
+        public ActionResult RegistrarProveedor()
+        {
+            return View();
+        }
+
+        // Acción para editar empleado
+        public ActionResult EditarProveedor()
+        {
+            return View();
+        }
+
+        // Acción para ver detalles de empleado
+        public ActionResult DetallesProveedor()
+        {
+            return View();
+        }
+
+    }
+}
