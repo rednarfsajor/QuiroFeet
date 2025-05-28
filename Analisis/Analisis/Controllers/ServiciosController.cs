604909432da27441990b66e291cab0460e282baa
jseara118 <jchavarria670331@ufide.ac.cr>
2025-05-28 17:52:44 -0600

    Squashed commit of the following:
    commit 3d314bf60cc3b71c0e5c10abfa353e18daff2e73
    Author: HeymmyLJuarez <hlopez68900@ufide.ac.cr>
    Date:   Wed May 28 12:04:06 2025 -0600
    
        Se realizan ajustes de BD y CRUD de servicios
    
        Se enlaza la BD en el código, adicional, se edita el controller de servicios para que el resto de pantallas de CRUD sean funcionales con la BD en SQL Server
    
    commit fc434c1e66c06e14d910f6cd423b938dca736a67
    Author: HeymmyLJuarez <hlopez68900@ufide.ac.cr>
    Date:   Tue May 27 17:59:21 2025 -0700
    
        Test de ajustes

--- Analisis/Analisis/Controllers/ServiciosController.cs
+++ Analisis/Analisis/Controllers/ServiciosController.cs
@@ -1,30 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Analisis.BD; // Tu contexto de base de datos
+using System.Data.Entity;
+
 
 namespace Analisis.Controllers
 {
     public class ServiciosController : Controller
     {
-        // GET: Servicios
+        private QuiroFeetEntities1 db = new QuiroFeetEntities1();
+
+
         public ActionResult ServiceIndex()
         {
             return View();
         }
 
+
+        // GET: Servicios/ListServices
+        public ActionResult ListServices()
+        {
+            var servicios = db.Servicios.ToList();
+            return View(servicios);
+        }
+
+        // GET: Servicios/CreateService
         public ActionResult CreateService()
         {
             return View();
         }
-        public ActionResult ListServices()
+
+        // POST: Servicios/CreateService
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateService(Servicios servicio)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Servicios.Add(servicio);
+                db.SaveChanges();
+                return RedirectToAction("ListServices");
+            }
+            return View(servicio);
         }
-        public ActionResult EditService()
+
+        // GET: Servicios/EditService/5
+        public ActionResult EditService(int id)
         {
-            return View();
+            var servicio = db.Servicios.Find(id);
+            if (servicio == null) return HttpNotFound();
+            return View(servicio);
+        }
+
+        // POST: Servicios/EditService/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditService(Servicios servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(servicio).State = EntityState.Modified; // 💡 Requiere el using
+                db.SaveChanges();
+                return RedirectToAction("ListServices");
+            }
+            return View(servicio);
+        }
+
+        // GET: Servicios/DeleteService/5
+        public ActionResult DeleteService(int id)
+        {
+            var servicio = db.Servicios.Find(id);
+            if (servicio == null) return HttpNotFound();
+
+            db.Servicios.Remove(servicio);
+            db.SaveChanges();
+            return RedirectToAction("ListServices");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
