3a2b3d3e3a445d23d629587962892b882b85c43f
HeymmyLJuarez <hlopez68900@ufide.ac.cr>
2025-06-19 20:46:34 -0600

    Ajustes de Roles de Usuario y Login

--- Analisis/Analisis/App_Start/RouteConfig.cs
+++ Analisis/Analisis/App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace Analisis
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+             name: "Default",
+             url: "{controller}/{action}/{id}",
+             defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+         );
+
+            routes.MapRoute(
+    name: "Default",
+    url: "{controller}/{action}/{id}",
+    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+);
+
         }
     }
 }
